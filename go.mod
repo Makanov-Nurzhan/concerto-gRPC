ac0662dc5f47ad8a0624e381bf61480d23c4018b
Маканов Нуржан Сабырович <makanov_n@cpi.nis.edu.kz>
2025-12-10 16:03:22 +0500

    added using operationID

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Makanov-Nurzhan/concerto-gRPC
 go 1.24.0
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	google.golang.org/grpc v1.77.0
 	google.golang.org/protobuf v1.36.10
--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,10 +3,12 @@ package domain
 import "errors"
 
 var (
-	ErrInvalidRefund        = errors.New("invalid refund")
-	ErrInvalidAttemptCount  = errors.New("invalid attempt count")
-	ErrInvalidTotalAttempts = errors.New("invalid total attempts")
-	ErrFirstDayAttempts     = errors.New("first day attempts")
-	ErrHasActiveSession     = errors.New("user has active session")
-	ErrFailedToUpdate       = errors.New("failed to update")
+	ErrInvalidRefund             = errors.New("invalid refund")
+	ErrInvalidAttemptCount       = errors.New("invalid attempt count")
+	ErrInvalidTotalAttempts      = errors.New("invalid total attempts")
+	ErrFirstDayAttempts          = errors.New("first day attempts")
+	ErrHasActiveSession          = errors.New("user has active session")
+	ErrFailedToUpdate            = errors.New("failed to update")
+	ErrOperationAlreadyProcessed = errors.New("operation already processed")
+	ErrOperationInProgress       = errors.New("operation is in progress")
 )
